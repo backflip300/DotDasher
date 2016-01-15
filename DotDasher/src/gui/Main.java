ee90cdfb076080fae279b6e5d6225957e40113ae
j00721 <EdwardSmart98@gmail.co.uk>
2016-01-15 13:16:55 +0000

    added sound

--- DotDasher/src/gui/Main.java
+++ DotDasher/src/gui/Main.java
@@ -14,6 +14,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -21,7 +25,11 @@ import javax.swing.JPanel;
 public class Main extends JPanel implements KeyListener {
 	int dotNum, score, iWidth, iHeight;
 	boolean move, fired;
-
+	double angle;
+	Random rand;
+	int sound = 500;
+	int time = 1000;
+	  SourceDataLine sdl;	
 	Arrow p1,p2;
 	ArrayList<Dot> dot = new ArrayList<Dot>();
 	ArrayList<Integer> lines = new ArrayList<Integer>();
@@ -31,7 +39,7 @@ public class Main extends JPanel implements KeyListener {
 		setPreferredSize(new Dimension(1000, 1000));
 		setFocusable(true);
 		addKeyListener(this);
-
+		rand = new Random();
 		p1 = new Arrow(500, 500, 50);
 	
 		dot.add(new Dot(250, 140, 50));
@@ -41,10 +49,37 @@ public class Main extends JPanel implements KeyListener {
 
 	// looping section which redraws frame
 	public void paint(Graphics g) {
+		  byte[] buf = new byte[ 1 ];;
+		    AudioFormat af = new AudioFormat( (float )44100, 8, 1, true, false );
+		  	
+				try {
+					sdl = AudioSystem.getSourceDataLine( af );
+				} catch (LineUnavailableException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+		    try {
+				sdl.open();
+			} catch (LineUnavailableException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		    sdl.start();
+		    for( int i = 0; i < time * (float )44100 / 1000; i++ ) {
+	angle = i / ( (float )44100 / sound ) * 2.0 * Math.PI;
+		        buf[ 0 ] = (byte )( Math.sin( angle ) * 100 );
+		        sdl.write( buf, 0, 1 );
+		        
+		    }
+		    sound = rand.nextInt(800) + 1;
+		    time = rand.nextInt(100) + 1;
+		    
+		    sdl.drain();
+		    sdl.stop();
 		g.setColor(Color.WHITE);
 		iWidth = getWidth();
 		iHeight = getHeight();
-		
+		 
 		g.fillRect(0, 0, iWidth, iHeight);
 		g.setColor(Color.black);
 		g.drawString("score:" + score, 50, 50);
@@ -55,6 +90,7 @@ public class Main extends JPanel implements KeyListener {
 		}
 		g.setColor(Color.RED);
 
+		
 		for (int i = 0; i < lines.size() / 4; i++) {
 			g.drawLine(lines.get(4 * i), lines.get(4 * i + 1),
 					lines.get(4 * i + 2), lines.get(4 * i + 3));
@@ -91,7 +127,7 @@ public class Main extends JPanel implements KeyListener {
 		f.setResizable(true);
 		f.add(main);
 		f.pack(); // f.setBackground(Color.black); // doesn't work
-		f.setTitle("physics engine");
+		f.setTitle("DotDasher");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setLocationRelativeTo(null);
 		f.setVisible(true);
